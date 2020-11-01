fdc3f85d52aeb6888c760baa7cc8e8cd589d4ab2
Jonathan Bisson <research@bjonnh.net>
2020-10-31 22:15:29 -0500

    Add documentation and doesn't require the cache file to exist to cache a new ontology (that was a bug).

--- src/main/kotlin/net/nprod/konbu/cache/FileCacheManager.kt
+++ src/main/kotlin/net/nprod/konbu/cache/FileCacheManager.kt
@@ -4,32 +4,50 @@ import ifExists
 import mu.KotlinLogging
 import java.io.File
 
+/**
+ * Handle cached files
+ */
 class FileCacheManager(private val cacheLocation: File) : CacheManager<File> {
     init {
         logger.info("Cache location: ${cacheLocation.path}")
         if (!cacheLocation.exists()) cacheLocation.mkdirs()
     }
 
+    /**
+     * Get the File for an entry, but doesn't check if it exists
+     */
     fun getEntryFile(entry: String): File {
         logger.debug("Getting entry $entry from cache.")
         return File(cacheLocation, entry)
     }
 
+    /**
+     * Checks if an entry exists in the cache
+     */
     override fun hasEntry(entry: String): Boolean {
         logger.debug("Checking entry $entry in cache.")
         return File(cacheLocation, entry).exists()
     }
 
+    /**
+     * Get an entry and throw an exception if it does not exist
+     */
     override fun getEntry(entry: String): File {
         val file = getEntryFile(entry)
         require(file.exists()) { "The entry $entry does not exist in the cache: ${file.path}." }
         return file
     }
 
+    /**
+     * Get an entry and return null if it does not exist
+     */
     override fun getEntryOrNull(entry: String): File? {
-        return getEntry(entry).ifExists()
+        return getEntryFile(entry).ifExists()
     }
 
+    /**
+     * Delete an entry in the cache (but not the file)
+     */
     override fun delete(entry: String): Boolean {
         logger.debug("Delete entry $entry in cache.")
         return getEntry(entry).delete()
